2fd09cf16224c121ad4c94fbb63661f6a63f871c
Mahmudul Arefin Rafi <mahmudularefinrafi@gmail.com>
2026-08-30 21:04:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,59 @@ sections.forEach(section => {
     observer.observe(section);
 
 });
+
+console.log("Mahmudul Arefin Rafi's Portfolio Loaded Successfully!");
+
+
+// Smooth Navigation
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (event) {
+
+        const targetId = this.getAttribute("href");
+
+        if (targetId === "#") return;
+
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+
+            event.preventDefault();
+
+            targetSection.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
+
+
+// Scroll Animation
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show-section");
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+
+sections.forEach(section => {
+    observer.observe(section);
+});
